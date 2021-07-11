f3285d147ac61846700d7ebe99916d7553ba5f08
Saligia-Vivaldi <it@mysklep.pl>
2021-07-11 21:02:25 +0200

    Update

--- base.sh
+++ base.sh
@@ -4,6 +4,7 @@
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc
 # Setup locale, language and keymap 
+sed -i '177s/.//' /etc/locale.gen # Activate en_US.UTF-8 UTF-8
 sed -i '390s/.//' /etc/locale.gen # Activate pl_PL.UTF-8 UTF-8
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
@@ -20,8 +21,8 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 # Setup root password
 echo root:password | chpasswd
 
-# Xorg should be installed with a desired WM/DE
-# If installing on laptop - TLP should be added
+# Xorg should be installed together with a desired WM/DE
+# Optionals are 'bluez bluez-utils cups hplip tlp'
 
 # Install packages
 pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
@@ -31,47 +32,26 @@ pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialo
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 # Install and configure GRUB (Watch out for os-probber if dualbooting)
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# self-explanatory
+# Enable services
 systemctl enable NetworkManager
-
-# self-explanatory
 systemctl enable bluetooth
-
-# Printer service (Common Unix Printing System - CUPS)
-systemctl enable cups.service
-
-# SSH daemon
-systemctl enable sshd
-
-# mDNS/DNS daemon
-systemctl enable avahi-daemon
-
-# Battery management service for TLP package
-systemctl enable tlp
-
-# Periodically refresh mirror list and sort by speed
-systemctl enable reflector.timer
-
-# Periodically conduct trim for SSD
-systemctl enable fstrim.timer
-
-# Virtualization
-systemctl enable libvirtd
-
-# self-explanatory
+systemctl enable cups.service		# Printer service
+systemctl enable sshd				# SSH daemon
+systemctl enable avahi-daemon		# mDNS/DNS daemon
+systemctl enable tlp				# Laptop Battery management
+systemctl enable reflector.timer	# Periodic mirrors list sorting by speed
+systemctl enable fstrim.timer		# Periodic trim for SSD
+systemctl enable libvirtd			# Virtualization
 systemctl enable firewalld
+systemctl enable acpid				# Advanced Configuration & Power Interface
 
-# Advanced Configuration and Power Interface
-systemctl enable acpid
-
-# Setup saligia user, add to sudoers
+# Setup user
 useradd -m saligia
 echo saligia:password | chpasswd
 usermod -aG libvirt saligia
 echo "saligia ALL=(ALL) ALL" >> /etc/sudoers.d/saligia
 
-
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m\n"
